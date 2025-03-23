96f57cf47474a479af9961cb5b8602ad8b919881
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-23 20:53:12 -0300

    feat: add Add screen to RootStackParamList and define ToDo type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
 export type RootStackParamList = {
   Home: undefined;
   Edit: undefined;
+  Add: undefined;
+};
+
+export type ToDo = {
+  name: string;
+  description?: string;
+  done: boolean;
 };
