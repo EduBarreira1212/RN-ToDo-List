a0830e2a24af99b71cb448d72878d729c28f6ee5
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-23 20:53:23 -0300

    feat: add Add screen to navigation stack

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,7 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
 import HomeScreen from '../screens/home';
-import EditScreen from '../screens/edit';
+import EditScreen from '../screens/Edit';
+import AddScreen from '../screens/Add';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,6 +14,7 @@ const Navigation = () => {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Edit" component={EditScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Add" component={AddScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
