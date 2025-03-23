b225e0a97d02511632477766ad6ed4f7ed3aa598
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-22 22:13:29 -0300

    feat: add styles for main container in home screen

---
+++ src/screens/home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        width: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+});
+
+export default styles;
