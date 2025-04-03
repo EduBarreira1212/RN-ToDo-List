7d34b3582c031ad93ead448d6764c0399252bdb9
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-02 23:33:13 -0300

    feat: implement custom hook for managing to-do list items and completed tasks

---
+++ src/hooks/useToDoList.ts
@@ -0,0 +1,98 @@
+import { useAtom } from "jotai";
+import atoms from "../atoms";
+import { ToDo } from "../types";
+
+type AddItemParameters = {
+    name: string;
+    description: string;
+};
+
+const useToDoList = () => {
+    const [items, setItems] = useAtom(atoms.items);
+  
+    const [completedItems, setCompletedItems] = useAtom(atoms.completedItems);
+  
+    const addItem = ({ name, description }: AddItemParameters) => {
+      setItems((s) => {
+        const newItem = {
+          name: name,
+          description: description,
+          done: false,
+        };
+  
+        const oldState = [...s];
+  
+        oldState.push(newItem);
+  
+        return oldState;
+      });
+    };
+  
+    const completeItem = (item: ToDo, index: number) => {
+      if (item.done) {
+        setItems((s) => {
+          const oldState = [...s];
+  
+          const itemToAdd = {
+            ...item,
+            done: false,
+          };
+  
+          oldState.push(itemToAdd);
+  
+          return oldState;
+        });
+  
+        setCompletedItems((s) => {
+          const oldState = [...s];
+  
+          oldState.splice(index, 1);
+  
+          return oldState;
+        });
+  
+        return;
+      }
+  
+      setItems((s) => {
+        const oldState = [...s];
+  
+        oldState.splice(index, 1);
+  
+        return oldState;
+      });
+  
+      setCompletedItems((s) => {
+        const oldState = [...s];
+  
+        const itemToAdd = {
+          ...item,
+          done: true,
+        };
+  
+        oldState.push(itemToAdd);
+  
+        return oldState;
+      });
+    };
+  
+    const removeItem = (index: number) => {
+      setItems((s) => {
+        const oldState = [...s];
+  
+        oldState.splice(index, 1);
+
+        return oldState;
+      });
+    };
+
+    return {
+        items,
+        completedItems,
+        addItem,
+        completeItem,
+        removeItem,
+      };
+    };
+    
+    export default useToDoList;
